ff6b8581f6320c2a98de9e9ce9f41bbb2101ebb6
m-murillo <si.marinamurillo@gmail.com>
2020-11-27 20:04:14 +0100

    [CMD] Date: Añadido awaitReactions

--- Commands/date.js
+++ Commands/date.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = 
 {
     name: 'date',
@@ -9,7 +11,80 @@ module.exports =
     execute(message, args)
     {
         if (args.length > 0){
-            message.channel.send('You went for a date with: ' + args.join(' '));
+            //Embed date
+            const dateEmbed = new Discord.MessageEmbed()
+            .setColor(rgbRandom())
+            .setTitle('Date with **' + args.join(' ') + '**')
+            .setDescription('Answer the question correctly for a reward!\nYou have 10s')
+            .setThumbnail('https://media.tenor.com/images/f812668358cc20abe799b85ddcc58aa1/tenor.gif')
+            .addFields(
+            { name: 'Which race ' + args.join(' ') + ' belongs to?', value: '1️⃣ Imanity\n2️⃣ Elf\n3️⃣ Werebeast\n4️⃣ Fairy'},
+            )
+
+            //Reaction into embedMessage
+            message.channel.send(dateEmbed).then(embedMessage => {
+                //To make reactions in order
+                embedMessage.react("1️⃣")
+                .then(() => embedMessage.react('2️⃣'))
+                .then(() => embedMessage.react('3️⃣'))
+                .then(() => embedMessage.react('4️⃣'))
+
+                //Take the info of the reactions
+                const filter = (reaction, user) => {
+                    return ['1️⃣', '2️⃣', '3️⃣', '4️⃣'].includes(reaction.emoji.name) && user.id === message.author.id;
+                };
+    
+                embedMessage.awaitReactions(filter, { max: 1, time: 15000, errors: ['time'] })
+                .then(collected => {
+                    const reaction = collected.first();
+    
+                    //If u react to the correct answer, edit the embed and show correct
+                    switch (reaction.emoji.name) {
+                        case '1️⃣':    
+                            //Create the new embed and edit
+                            embedMessage.edit(embedWin())
+                            break;
+                        case '2️⃣':
+                            embedMessage.edit(embedFail('2️⃣'))
+                            break;
+                        case '3️⃣':
+                            embedMessage.edit(embedFail('3️⃣'))
+                            break;
+                        case '4️⃣':
+                            embedMessage.edit(embedFail('4️⃣'))
+                            break;
+                    }
+                })
+                //If there is no reply
+                .catch(collected => {
+                    embedMessage.edit(embedTimeUp())
+                });
+            });
         }
-    }
+    }    
+}
+
+function rgbRandom() {
+    return '#0000ff'
+}
+
+function embedWin() {
+    let embedWin = new Discord.MessageEmbed()
+    .setDescription('Correct!')
+    .setColor("#00ff00")
+    return embedWin;
+}
+
+function embedFail(option) {
+    let embedFail = new Discord.MessageEmbed()
+    .setDescription('Fail! You choose ' + option)
+    .setColor("#ff0000")
+    return embedFail;
+}
+
+function embedTimeUp(){
+    let embedTimeUp = new Discord.MessageEmbed()
+    .setDescription('Time\'s up!')
+    .setColor("#000000")
+    return embedTimeUp;
 }
